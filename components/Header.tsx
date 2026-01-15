556529388185873b355b84ab179bc6866be2fb50
xorms571 <xorms6865@naver.com>
2026-01-15 12:45:23 +0900

    feat: 의존성 업데이트 및 SVG 에셋 수정
    - package.json에 @radix-ui/themes 의존성을 추가했습니다.
    - 사용하지 않는 SVG 파일(file.svg, globe.svg, next.svg, vercel.svg, window.svg)을 삭제했습니다.
    - 새로운 SVG 파일(logo.svg, plus.svg, submit-star.svg, user.svg)을 추가했습니다.
    - Header 컴포넌트 추가

---
+++ components/Header.tsx
@@ -0,0 +1,53 @@
+import { Button, IconButton, SegmentedControl } from "@radix-ui/themes";
+import Image from "next/image";
+import Link from "next/link";
+
+interface props {
+    isLoggedIn: boolean;
+    creditAmount: number;
+    userName: string;
+    userAvatar?: string;
+}
+
+export default function Header({ isLoggedIn, creditAmount, userName, userAvatar }: props) {
+    return (
+        <header className="w-full h-20 p-4 flex justify-between items-center">
+            <div className="flex items-center gap-14">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="Logo" width={133} height={22} />
+                </Link>
+                <SegmentedControl.Root defaultValue="inbox">
+                    <SegmentedControl.Item value="inbox">LOOK BOOK</SegmentedControl.Item>
+                    <SegmentedControl.Item value="drafts">GALLERY</SegmentedControl.Item>
+                </SegmentedControl.Root>
+            </div>
+            {!isLoggedIn ? (
+                <Button variant="solid" className="bg-(--color-main)!">
+                    LOGIN
+                </Button>
+            ) : (
+                <div className="flex items-center gap-6">
+                    <Button variant="solid" className="bg-(--color-main)!">
+                        Submit Prompt <Image src="/submit-star.svg" alt="Submit Prompt" width={15} height={15} />
+                    </Button>
+                    <div className="flex items-center gap-1">
+                        <div className="min-w-24 min-h-8 leading-8 text-center border border-gray-200 rounded-sm">
+                            {creditAmount} C
+                        </div>
+                        <IconButton className="bg-(--color-main)!">
+                            <Image src='/plus.svg' alt='Add Credit' width={18} height={18} />
+                        </IconButton>
+                    </div>
+                    <div className="flex items-center gap-2 font-bold">
+                        {!userAvatar ?(
+                            <Image src="/user.svg" alt="User Avatar" width={32} height={32} />
+                        ):(
+                            <Image className="rounded-full" src={userAvatar} alt="User Avatar" width={32} height={32} />
+                        )}
+                        {userName}
+                    </div>
+                </div>
+            )}
+        </header>
+    );
+}
