4cb9ad18d1d5215e76b83211b3e97dff99bb4ad1
xorms571 <83347911+xorms571@users.noreply.github.com>
2026-01-16 06:04:41 +0900

    Develop (#2)
    * feat: 의존성 업데이트 및 SVG 에셋 수정
    
    - package.json에 @radix-ui/themes 의존성을 추가했습니다.
    - 사용하지 않는 SVG 파일(file.svg, globe.svg, next.svg, vercel.svg, window.svg)을 삭제했습니다.
    - 새로운 SVG 파일(logo.svg, plus.svg, submit-star.svg, user.svg)을 추가했습니다.
    - Header 컴포넌트 추가
    
    * feat: 검색 컴포넌트 구현 및 정렬 옵션 추가, 레이아웃 업데이트
    
    * feat: 갤러리 화면 및 목업 데이터(룩북이미지, 프롬프트)
    
    * feat: Lookbook 컴포넌트 구현 및 Gallery 뷰 전환 기능
    
    * feat: 메타데이터를 업데이트, Header 컴포넌트의 레이아웃을 Suspense를 사용하여 개선
    
    * feat: “Gallery와 Lookbook 컴포넌트를 데이터 props를 받을 수 있도록 리팩터링하고, Home 컴포넌트에 로딩 상태를 구현
    
    * style:글로벌 스타일 업데이트 및 새로운 SVG 아이콘 추가
    
    globals.css에서 메인 색상 스킴을 변경하여 --color-main을 #FF3400으로 업데이트하고, 포인트 및 호버 효과용 새로운 색상 변수를 추가했습니다.
    
    * style: 헤더 스타일 변경
    
    * style: 글로벌 스타일 업데이트, SVG 아이콘, Searching 컴포넌트 업데이트

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,38 +27,46 @@ export default function Header({ isLoggedIn, creditAmount, userName, userAvatar 
         <header className="w-full h-20 p-4 flex justify-between items-center">
             <div className="flex items-center gap-14">
                 <Link href="/">
-                    <Image src="/logo.svg" alt="Logo" width={133} height={22} />
+                    <Image src="icon/logo.svg" alt="Logo" width={133} height={22} />
                 </Link>
-                <SegmentedControl.Root value={currentView} onValueChange={handleViewChange}>
-                    <SegmentedControl.Item value="lookbook">LOOK BOOK</SegmentedControl.Item>
-                    <SegmentedControl.Item value="gallery">GALLERY</SegmentedControl.Item>
+                <SegmentedControl.Root radius="full" value={currentView} onValueChange={handleViewChange}>
+                    <SegmentedControl.Item value="lookbook" className="cursor-pointer!">
+                        {currentView !== "lookbook" ?
+                            <Image className="inline-block mr-1" src='icon/lookbook-inactive.svg' alt='Lookbook Inactive' width={18} height={18} /> :
+                            <Image className="inline-block mr-1" src='icon/lookbook-active.svg' alt='Lookbook Active' width={18} height={18} />} LOOK BOOK
+                    </SegmentedControl.Item>
+                    <SegmentedControl.Item value="gallery" className="cursor-pointer!">
+                        {currentView !== "gallery" ?
+                            <Image className="inline-block mr-1" src='icon/gallery-inactive.svg' alt='Gallery Inactive' width={18} height={18} /> :
+                            <Image className="inline-block mr-1" src='icon/gallery-active.svg' alt='Gallery Active' width={18} height={18} />} GALLERY
+                    </SegmentedControl.Item>
                 </SegmentedControl.Root>
             </div>
             {!isLoggedIn ? (
-                <Button variant="solid" className="bg-(--color-main)!">
+                <Button variant="solid" className="bg-(--color-main)! cursor-pointer!">
                     LOGIN
                 </Button>
             ) : (
                 <div className="flex items-center gap-6">
-                    <Button variant="solid" className="bg-(--color-main)!">
-                        Submit Prompt <Image src="/submit-star.svg" alt="Submit Prompt" width={15} height={15} />
+                    <Button variant="solid" className="rounded-4xl! bg-(--color-main)! cursor-pointer!">
+                        Submit Prompt <Image src="icon/submit-star.svg" alt="Submit Prompt" width={15} height={15} />
                     </Button>
-                    <div className="flex items-center gap-1">
-                        <div className="min-w-24 min-h-8 leading-8 text-center border border-gray-200 rounded-sm">
-                            {creditAmount} C
+                    <div className="flex items-center gap-1 border border-gray-200 rounded-4xl overflow-hidden">
+                        <div className="flex justify-between min-w-24 min-h-8 leading-8 text-center">
+                            <div className="flex justify-center gap-1 ml-4 mr-2">{creditAmount} <Image src="icon/credit.svg" alt="Credit" width={18} height={18} /></div>
+                            <IconButton className="bg-(--color-main)! rounded-4xl! cursor-pointer!">
+                                <Image className="inline-block" src='icon/plus.svg' alt='Add Credit' width={18} height={18} />
+                            </IconButton>
                         </div>
-                        <IconButton className="bg-(--color-main)!">
-                            <Image src='/plus.svg' alt='Add Credit' width={18} height={18} />
-                        </IconButton>
                     </div>
-                    <div className="flex items-center gap-2 font-bold">
-                        {!userAvatar ?(
-                            <Image src="/user.svg" alt="User Avatar" width={32} height={32} />
-                        ):(
+                    <Link href="/profile" className="flex items-center gap-2 font-bold">
+                        {!userAvatar ? (
+                            <Image src="icon/user.svg" alt="User Avatar" width={32} height={32} />
+                        ) : (
                             <Image className="rounded-full" src={userAvatar} alt="User Avatar" width={32} height={32} />
                         )}
                         {userName}
-                    </div>
+                    </Link>
                 </div>
             )}
         </header>
