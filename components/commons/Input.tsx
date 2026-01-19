61010bbbe9161ff5cc76f8297af4fba12746c444
xorms571 <xorms6865@naver.com>
2026-01-19 14:26:59 +0900

    fix: 인풋 높이 조정, 검색 아이콘 레이아웃 개선

--- components/commons/Input.tsx
+++ components/commons/Input.tsx
@@ -6,7 +6,7 @@ import React from "react";
 type InputVariants = VariantProps<typeof inputVariants>;
 
 const inputVariants = cva(
-    "min-h-10 w-full flex items-center rounded-4xl border-1 text-body1-medium bg-gray-50 hover:bg-gray-100 focus:bg-gray-50 focus:border-[1.5px] focus:border-(--color-main) active:text-gray-900 text-gray-900",
+    "h-11 w-full flex items-center rounded-4xl border-1 text-body1-medium bg-gray-50 hover:bg-gray-100 focus:bg-gray-50 focus:border-[1.5px] focus:border-(--color-main) active:text-gray-900 text-gray-900",
     {
         variants: {
             border: {
@@ -37,12 +37,12 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     ({ className, isSearching = false, border, radius, ...props }, ref) => {
         return (
             <div className={cn(inputVariants({ border, radius }), className, { "text-gray-600": isSearching })}>
-                {isSearching && <div className="px-1.25 mr-3.5">
-                    <Image src="/icon/magnifying-glass-icon.svg" alt="Search Icon" width={30} height={30} />
+                {isSearching && <div className="ml-2 h-7.75 min-w-7.75 relative">
+                    <Image src="/icon/magnifying-glass-icon.svg" alt="Search Icon" fill className="h-7.75 left-0!" />
                 </div>}
                 <input
                     ref={ref}
-                    className={`${isSearching ? "mr-4.5" : "mx-4.5"} w-full outline-none placeholder:text-gray-600`}
+                    className="mx-4.75 w-full outline-none placeholder:text-gray-600"
                     {...props}
                 />
             </div>
