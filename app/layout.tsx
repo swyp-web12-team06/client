ac76dae97dd189f384c8c784d6bf0c72dbe07be7
xorms571 <xorms6865@naver.com>
2026-01-21 07:35:30 +0900

    fix: 레이아웃 머지

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import { Theme } from "@radix-ui/themes";
-import "./globals.css";
-import Header from "@/components/Header";
-import { Suspense } from "react";
-import { AuthProvider } from "@/context/AuthContext";
-=======
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import './globals.css';
@@ -14,7 +5,6 @@ import GlobalHeader from '@/components/GlobalHeader';
 import { Suspense } from 'react';
 import { AuthProvider } from '@/context/AuthContext';
 import './globals.css';
->>>>>>> 34dd7b48b790a175ce2f15d47e9a0be47138f22f
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -30,17 +20,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-<<<<<<< HEAD
-    <html lang="en" className={suit.variable}>
-      <body className="antialiased">
-        <AuthProvider>
-          <Theme accentColor="tomato" className="max-w-7xl m-auto">
-            <Suspense fallback={<div className="h-20 w-full"></div>}>
-              <Header />
-            </Suspense>
-            {children}
-          </Theme>
-=======
     <html lang="en" className={roboto.variable}>
       <body className="p-4 antialiased">
         <AuthProvider>
@@ -48,7 +27,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <GlobalHeader />
           </Suspense>
           {children}
->>>>>>> 34dd7b48b790a175ce2f15d47e9a0be47138f22f
         </AuthProvider>
       </body>
     </html>
