981c12dedfd4bb62193b505ec83fe4cc42c9b3c6
xorms571 <xorms6865@naver.com>
2026-01-29 10:53:35 +0900

    feat: 갤러리 및 룩북 구성 요소를 업데이트하여 상품 유형을 사용하고 렌더링 로직을 개선

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
-import { LookbookImage } from '@/type/lookbook';
+import { Product } from '@/type/product';
+import Link from 'next/link';
 import { useMemo } from 'react';
 
-export default function Gallery({ data }: { data: LookbookImage[] }) {
+export default function Gallery({ data }: { data: Product[] }) {
+  // 매소너리 ui구현을 위한 랜덤 높이 함수
   const randomHeights = useMemo(() => {
     const arr = [300, 350, 400, 450, 500, 550, 600];
     return data.map(() => arr[Math.floor(Math.random() * arr.length)]);
@@ -10,24 +11,28 @@ export default function Gallery({ data }: { data: LookbookImage[] }) {
 
   return (
     <div className="columns-2 gap-4 space-y-4 md:columns-3 lg:columns-4">
-      {data.map((image, index) => (
-        <div
-          key={image.lookbook_image_id}
-          className="group relative break-inside-avoid overflow-hidden rounded-xl"
-          style={{ height: randomHeights[index] }}
-        >
-          <Image
-            src={''}
-            alt={String(image.lookbook_image_id)}
-            fill
-            className="bg-gray-400 object-cover transition-transform duration-300 group-hover:scale-105"
-            loading="lazy"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <p>{image.prompt_id}</p>
+      {data.map((product, index) => {
+        return (
+          <div
+            key={product.promptId}
+            className="group relative break-inside-avoid overflow-hidden rounded-xl"
+            style={{ height: randomHeights[index] }}
+          >
+            <Link href={`/lookbook/${product.promptId}`} key={product.promptId} className="break-inside-avoid">
+            <img
+              src={product.previewImageUrl}
+              alt={product.title}
+              className="bg-gray-200 object-cover transition-transform duration-300 group-hover:scale-105"
+            />
+            <div className="absolute inset-0 bg-gray-400 bg-opacity-0 group-hover:bg-opacity-20 transition-all duration-300" />
+            <div className="absolute bottom-0 left-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <p className="font-bold">{product.title}</p>
+              <p className="text-sm">{product.seller.nickname}</p>
+            </div>
+            </Link>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   );
 }
