32c3f74b8c3b1c41e9653445723f815ec82b98ee
xorms571 <xorms6865@naver.com>
2026-02-02 17:48:54 +0900

    fix: 소셜 로그인 모달 컴포넌트 적용

--- components/GlobalHeader.tsx
+++ components/GlobalHeader.tsx
@@ -33,7 +33,7 @@ export default function GlobalHeader() {
 
   return (
     <>
-      <header className="fixed w-[calc(100%-32px)] top-3 z-15 flex items-center justify-between rounded-3xl bg-white px-7 py-3 shadow-[0px_0px_10px_0px_rgba(20,20,20,0.10)]">
+      <header className="fixed top-3 z-15 flex w-[calc(100%-32px)] items-center justify-between rounded-3xl bg-white px-7 py-3 shadow-[0px_0px_10px_0px_rgba(20,20,20,0.10)]">
         <div className="inline-flex items-center gap-8">
           <Link href="/">
             <Image src="/icon/logo.svg" alt="Logo" width={133} height={22} />
@@ -50,7 +50,9 @@ export default function GlobalHeader() {
             </Button>
             <Button
               className={`w-40 bg-transparent text-gray-500 transition-none ${currentView == 'gallery' ? 'bg-gray-900 text-gray-50 hover:bg-gray-900' : 'hover:bg-transparent hover:text-gray-600'}`}
-              prefixIcon={currentView !== 'gallery' ? <GalleryInActiveIcon /> : <GalleryActiveIcon />}
+              prefixIcon={
+                currentView !== 'gallery' ? <GalleryInActiveIcon /> : <GalleryActiveIcon />
+              }
               onClick={() => handleViewChange('gallery')}
             >
               갤러리
@@ -87,7 +89,7 @@ export default function GlobalHeader() {
           )}
         </div>
       </header>
-      {isModalOpen && <SocialLoginModal onClose={closeModal} />}
+      <SocialLoginModal isOpen={isModalOpen} onClose={closeModal} />
     </>
   );
 }
