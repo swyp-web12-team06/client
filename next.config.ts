4db95a121b2a4a7205baa0304e4851d1f1af1b2a
xorms571 <83347911+xorms571@users.noreply.github.com>
2026-02-04 19:58:36 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         // source: a pattern to match against incoming requests
         source: '/api/:path*',
         // destination: the URL to proxy the request to
-        destination: 'https://test.redot.store/:path*',
+        destination: '@backend/api/:path*',
       },
     ];
   },
