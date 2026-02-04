10f9874f510a9d6ac9d584bd5611820f44d0d2fe
xorms571 <xorms6865@naver.com>
2026-02-04 19:09:38 +0900

    fix: 도메인 변경

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         // source: a pattern to match against incoming requests
         source: '/api/:path*',
         // destination: the URL to proxy the request to
-        destination: 'http://localhost:8080/:path*',
+        destination: 'https://redot.store/:path*',
       },
     ];
   },
