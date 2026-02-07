257048ca4ad2c8b687c5594d1d7fc25b5c4b7703
merx88 <wjang07@gmail.com>
2026-02-07 23:34:18 +0900

    feat: 이미지 상세 디자인 완료

--- type/product.ts
+++ type/product.ts
@@ -1,20 +1,31 @@
+type Image = {
+  id: number;
+  imageUrl: string;
+  isPreview: boolean;
+  isRepresentative: boolean;
+  optionValues: {
+    [key: string]: string;
+  };
+};
+
 export type Product = {
-    promptId: number;
-    title: string;
-    description: string;
-    price: number;
-    userStatus: string;
-    categoryId: number;
-    categoryName: string;
-    modelId: number;
-    modelName: string;
-    previewImageUrl: string;
-    representativeImageUrls: string[];
-    tags: string[];
-    seller: {
-        id: number;
-        nickname: string;
-    };
-    createdAt: string;
-    updatedAt: string;
-}
+  promptId: number;
+  title: string;
+  description: string;
+  price: number;
+  userStatus: string;
+  categoryId: number;
+  categoryName: string;
+  modelId: number;
+  modelName: string;
+  previewImageUrl: string;
+  representativeImageUrls: string[];
+  tags: string[];
+  images: Image[];
+  seller: {
+    id: number;
+    nickname: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+};
