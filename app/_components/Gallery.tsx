6ca20dc20d73305e0152acfba712f8fb24499577
xorms571 <xorms6865@naver.com>
2026-02-09 00:58:24 +0900

    fix: 수정

--- app/_components/Gallery.tsx
+++ app/_components/Gallery.tsx
@@ -16,18 +16,18 @@ export default function Gallery({ data }: { data: Product[] }) {
   }
 
   return (
-    <div className="columns-2 gap-4 space-y-4 md:columns-3 lg:columns-4">
+    <div className="columns-2 gap-4 md:columns-3 lg:columns-4">
       {data.map((product, idx) => {
         return (
           <div
             key={product.promptId}
-            className="group relative break-inside-avoid overflow-hidden rounded-xl"
+            className="group relative mb-4 break-inside-avoid overflow-hidden rounded-xl"
             onClick={() => handleProductDetail(product, idx)}
           >
             <img
               src={product.previewImageUrl}
               alt={product.title}
-              className="w-full bg-gray-500 object-cover transition-transform duration-300 group-hover:scale-105"
+              className="h-full w-full bg-gray-500 object-cover transition-transform duration-300 group-hover:scale-105"
             />
           </div>
         );
